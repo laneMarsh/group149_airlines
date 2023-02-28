520d36271c209728389121e8d3a615c6d1297e9f
Lane Marsh <lmmarsh43@gmail.com>
2023-02-27 17:23:24 -0800

    adding stuff

---
+++ database/G1A_DDL.sql
@@ -0,0 +1,227 @@
+DROP TABLE repairs;
+DROP TABLE removals;
+DROP TABLE inventory_locations;
+DROP TABLE aircraft_parts;
+DROP TABLE aircraft;
+DROP TABLE parts;
+
+CREATE OR REPLACE TABLE parts (
+	part_number varchar(45) NOT NULL,
+    name varchar(45) NOT NULL,
+    description varchar(45) NOT NULL,
+    PRIMARY KEY(part_number)
+);
+
+CREATE OR REPLACE TABLE aircraft (
+	id_aircraft varchar(45) NOT NULL,
+    model varchar(45) NOT NULL,
+    PRIMARY KEY(id_aircraft)
+);
+
+CREATE OR REPLACE TABLE aircraft_parts (
+	id_aircraft varchar(45) NOT NULL,
+    part_number varchar(45) NOT NULL,
+    FOREIGN KEY(id_aircraft) REFERENCES aircraft(id_aircraft),
+    FOREIGN KEY(part_number) REFERENCES parts(part_number)
+);
+
+CREATE OR REPLACE TABLE inventory_locations(
+	id_inventory int NOT NULL AUTO_INCREMENT,
+    serial_number varchar(45) NOT NULL,
+    location varchar(45) NOT NULL,
+    part_number varchar(45),
+    PRIMARY KEY(id_inventory),
+    FOREIGN KEY(part_number) REFERENCES parts(part_number)
+);
+
+CREATE OR REPLACE TABLE removals(
+	id_removal int NOT NULL AUTO_INCREMENT,
+    removal_date datetime NOT NULL,
+    replacement_date datetime,
+    id_aircraft varchar(45) NOT NULL,
+    removed_part varchar(45) NOT NULL,
+    installed_part varchar(45),
+    PRIMARY KEY (id_removal),
+    FOREIGN KEY(id_aircraft) REFERENCES aircraft(id_aircraft),
+    FOREIGN KEY(removed_part) REFERENCES parts(part_number),
+    FOREIGN KEY(installed_part) REFERENCES parts(part_number)
+);
+
+CREATE OR REPLACE TABLE repairs (
+	id_repair int NOT NULL AUTO_INCREMENT,
+    recieved datetime NOT NULL,
+    completed datetime,
+    id_removal int NOT NULL,
+    supplier varchar(45) NOT NULL,
+    price float(10,2),
+    PRIMARY KEY(id_repair),
+    FOREIGN KEY(id_removal) REFERENCES removals(id_removal)
+);
+
+INSERT INTO parts (
+    part_number,
+    name,
+    description
+)
+VALUES 
+(
+    "123A4567-1",
+    "Fuselage",
+    "Boeing's single aisle, aerodynamic tube where the aircraft payload goes"
+),
+(
+    "987A6543-1",
+    "Left Wing",
+    "Provides lift and attaches to the left side of a B737 fuselage"
+),
+(
+    "987A6543-2",
+    "Right Wing",
+    "Provides lift and attaches to the right side of a B737 fuselage"
+);
+
+INSERT INTO aircraft (
+    id_aircraft,
+    model
+)
+VALUES 
+(
+    "G1A-01",
+    "B737"
+),
+(
+    "G1A-02",
+    "B737"
+),
+(
+    "G1A-03",
+    "B737"
+),
+(
+    "G1A-04",
+    "B737"
+),
+(
+    "G1A-05",
+    "B737"
+),
+(
+    "G1A-06",
+    "B737"
+),
+(
+    "G1A-51",
+    "A320"
+),
+(
+    "G1A-53",
+    "A320"
+);
+
+INSERT INTO aircraft_parts (
+    part_number,
+    id_aircraft
+)
+VALUES
+(
+    "123A4567-1",
+    "G1A-01"
+),
+(
+    "987A6543-1",
+    "G1A-01"
+),
+(
+    "987A6543-2",
+    "G1A-01"
+);
+
+INSERT INTO inventory_locations (
+    serial_number,
+    part_number,
+    location
+)
+VALUES
+(
+    "abc123",
+    "123A4567-1",
+    "G1A-01"
+),
+(
+    "abc123",
+    "987A6543-1",
+    "G1A-01"
+),
+(
+    "abc123",
+    "987A6543-2",
+    "G1A-01"
+),
+(
+    "abc124",
+    "123A4567-1",
+    "G1A-02"
+),
+(
+    "abc125",
+    "987A6543-1",
+    "G1A-02"
+),
+(
+    "abc126",
+    "987A6543-2",
+    "G1A-02"
+);
+
+INSERT INTO removals (
+    removal_date,
+    replacement_date,
+    id_aircraft,
+    removed_part,
+    installed_part
+)
+VALUES
+(
+    "2023-02-01",
+    "2018-02-02",
+    "G1A-01",
+    "987A6543-1",
+    "987A6543-1"
+),
+(
+    "2023-02-09",
+    NULL,
+    "G1A-02",
+    "987A6543-2",
+    NULL
+);
+
+INSERT INTO repairs (
+    recieved,
+    completed,
+    id_removal,
+    supplier,
+    price
+)
+VALUES
+(
+    "2023-02-02",
+    "2018-02-09",
+    "1",
+    "Spirit Aerospace",
+    "350.00"
+),
+(
+    "2023-02-09",
+    NULL,
+    "2",
+    "Spirit Aerospace",
+    NULL
+);
+
+SELECT * FROM parts;
+SELECT * FROM aircraft;
+SELECT * FROM repairs;
+SELECT * FROM removals;
+SELECT * FROM inventory_locations;
+SELECT * FROM aircraft_parts;
---
+++ database/G1A_DML.sql
@@ -0,0 +1,110 @@
+-- -------------parts---------------
+-- Create
+INSERT INTO parts (name, capacity) 
+VALUES (:part_number_input, :name_input, :description_input);
+
+-- Read
+SELECT * 
+FROM parts;
+
+-- Update
+UPDATE parts
+SET name = :name_input, capacity = :capacityInput, description = :description_input
+WHERE part_number = :part_number_input;
+
+-- Delete
+DELETE FROM parts WHERE part_number = :part_number_input;
+
+
+-- -------------aircraft---------------
+-- Create
+INSERT INTO aircraft (id_aircraft, model) 
+VALUES (:id_aircraft_input, :model_input);
+
+-- Read
+SELECT * 
+FROM aircraft;
+
+-- Update
+UPDATE aircraft
+SET model = :model_input
+WHERE id_aircraft = :id_aircraft_input;
+
+-- Delete
+DELETE FROM aircraft WHERE aircraft_id = :aircraft_id_input;
+
+
+-- -------------repairs---------------
+-- Create
+INSERT INTO repairs (recieved, id_removal, supplier) 
+VALUES (:recieved_input, :id_removal_dropDown, :supplier_input);
+
+-- Read
+SELECT repairs.id_repair, repairs.recieved, repairs.completed, removals.id_removal, repairs.supplier, repairs.price
+FROM repairs
+INNER JOIN removals ON repairs.id_removal = removals.id_removal
+ORDER BY recieved DESC;
+
+-- Update
+UPDATE repairs
+SET recieved = :recieved_input, completed = :completed_input, id_removal = :id_removal_dropDown, supplier = :supplier_input, price = :price_input
+WHERE id_repair = :id_repair_input;
+
+-- Delete
+DELETE FROM repairs WHERE id_repair = :id_repair_input;
+
+
+-- -------------removals---------------
+-- Create
+INSERT INTO removals (removal_date, id_aircraft, removed_part) 
+VALUES (:removal_date_input, :id_aircraft_dropDown, :removed_part_dropDown);
+
+-- Read
+SELECT * 
+FROM removals
+INNER JOIN aircraft ON removals.id_aircraft = aircraft.id_aircraft
+INNER JOIN parts AS parts1 ON removals.removed_part = parts1.part_number
+INNER JOIN parts AS parts2 ON removals.installed_part = parts2.part_number
+ORDER BY removal_date DESC;
+
+-- Update
+UPDATE removals
+SET removal_date = :removal_date_input, replacement_date = :replacement_date_input, id_aircraft = :id_aircraft_dropDown, removed_part = :removed_part_dropDown, installed_part = :installed_part_dropDown
+WHERE id_removal = :id_removal_input;
+
+-- Delete
+DELETE FROM Ports WHERE id_removal = :id_removal_input;
+
+
+-- -------------inventory_locations---------------
+-- Create
+INSERT INTO inventory_locations (serial_number, location, part_number) 
+VALUES (:serial_number_input, :location_input, :part_number_dropDown);
+
+-- Read
+SELECT inventory.id_inventory, inventory.serial_number, inventory.location, parts.part_number
+FROM inventory_locations AS inventory
+INNER JOIN parts ON inventory.part_number = parts.part_number;
+
+-- Update
+UPDATE inventory_locations
+SET serial_number = :serial_number_input, location = :location_input, part_number = :part_number_dropDown
+WHERE id_inventory = :id_inventory_input;
+
+-- Delete
+DELETE FROM inventory_locations WHERE id_inventory = :id_inventory_input;
+
+
+-- -------------aircraft_parts---------------
+-- Create
+INSERT INTO aircraft_parts (id_aircraft, part_number) 
+VALUES (:id_aircraft_dropDown, :part_number_dropDown);
+
+-- Read
+SELECT aircraft.aircraft_id AS id_aircraft, parts.part_number AS part_number
+FROM aircraft_parts
+INNER JOIN aircraft ON aircraft_parts.id_aircraft = aircraft.id_aircraft
+INNER JOIN parts ON aircraft_parts.part_number = parts.part_number;
+
+-- Delete
+DELETE FROM aircraft_parts WHERE id_aircraft = :id_aircraft_dropDown AND part_number = :part_number_dropDown;
