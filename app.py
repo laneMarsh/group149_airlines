0285ad4e116614c54a61f3f81f7400959564722c
Lane Marsh <lmmarsh43@gmail.com>
2023-02-27 18:39:48 -0800

    adding a bunch of stuff. The aircraft page is reading from my osu sql now.

--- app.py
+++ app.py
@@ -1,46 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json
+import database.db_connector as db
 import os
 
 # Configuration
 app = Flask(__name__)
 
-people_from_app_py = [
-{
-    "name": "Thomas",
-    "age": 33,
-    "location": "New Mexico",
-    "favorite_color": "Blue"
-},
-{
-    "name": "Gregory",
-    "age": 41,
-    "location": "Texas",
-    "favorite_color": "Red"
-},
-{
-    "name": "Vincent",
-    "age": 27,
-    "location": "Ohio",
-    "favorite_color": "Green"
-},
-{
-    "name": "Alexander",
-    "age": 29,
-    "location": "Florida",
-    "favorite_color": "Orange"
-}
-]
+db_connection = db.connect_to_database()
 
 # Routes 
 @app.route('/')
 def root():
-    return render_template("main.j2", people=people_from_app_py)
+    return render_template("index.j2")
+
+@app.route('/aircraft')
+def aircraft_page():
+    query = "SELECT * FROM aircraft;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("aircraft.j2", aircraft=results)
+    # return results
 
 # Listener
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 9112)) 
-    #                                 ^^^^
-    #              You can replace this number with any valid port
     
     app.run(port=port, debug=True)
