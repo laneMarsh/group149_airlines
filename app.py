5f519da030dce9f6542c27ea75e61fd8ebb70504
Lane Marsh <lmmarsh43@gmail.com>
2023-02-28 19:07:11 -0800

    aircraft page now can add aircraft and navigate back to the aircraft browse page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json
+from flask import Flask, render_template, json, request, redirect
 import database.db_connector as db
 import os
 
@@ -12,17 +12,29 @@ db_connection = db.connect_to_database()
 def root():
     return render_template("index.j2")
 
-@app.route('/aircraft')
+@app.route('/aircraft', methods=['POST', 'GET'])
 def aircraft_page():
-    query = "SELECT * FROM aircraft;"
-    cursor = db.execute_query(db_connection=db_connection, query=query)
-    results = cursor.fetchall()
-    return render_template("aircraft.j2", aircraft=results)
-    # return results
+    if request.method == "GET":
+        ac_query = "SELECT * FROM aircraft;"
+        cur = db.execute_query(db_connection=db_connection, query=ac_query)
+        ac_table = cur.fetchall()
+        return render_template("aircraft/aircraft.j2", aircraft=ac_table)
+    
+
+@app.route('/aircraft/create', methods=['POST', 'GET'])
+def add_aircraft_page():
+    if request.method == "GET":
+        return render_template("aircraft/add_aircraft.j2")
+
+    if request.method == "POST":
+        aircraftID = request.form['aircraftID']
+        model = request.form['model']
+        add_ac_query = 'INSERT INTO aircraft (id_aircraft, model) VALUES ("' + aircraftID + '", "' + model + '");'
+        if aircraftID != "" and model != "":
+            cur = db.execute_query(db_connection=db_connection, query=add_ac_query)
+        return redirect('/aircraft')
 
 # Listener
-
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 9112)) 
-    
+    port = int(os.environ.get('PORT', 45434)) 
     app.run(port=port, debug=True)
